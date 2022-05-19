9d6cf7e51429d5bfea25a5e5947ed616b58cdb3f
Bonaventura <javier.bonaventura@enel.com>
2022-05-19 08:28:51 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Entregable numero 1 Clase 2 de ReactJs
         </p>
         <a
           className="App-link"
