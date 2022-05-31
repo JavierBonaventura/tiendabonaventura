ba042d568f237be41872ade509a384e35f608338
Bonaventura <javier.bonaventura@enel.com>
2022-05-31 14:42:15 -0300

    Entregable 4

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,52 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class ItemCount extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numero: props.initial,
-    };
-  }
+export default function ItemCount({ stock, initial }) {
+  const [contador, setContador] = useState(initial);
+  
+  
+  const incrementar= () => {
+    setContador(contador <stock ? contador + 1 : contador);
+  };
 
- 
-    
+  //contador == 5 ? alert("Se alcanzo el maximo") : console.log("first")
 
-incrementar() {
-    if (this.state.numero < 5) {
-    this.setState({ numero: this.state.numero + 1 });
-  }
-}
+  const decrementar= () => {
+    setContador(contador >initial ? contador - 1 : contador);
+  };
 
-
-  decrementar() {
-    if (this.state.numero > 1) {
-    this.setState({ numero: this.state.numero - 1 });
-  }
-  }
-  render() {
-    return (
-         <div className="d-flex justify-content-center">
-         <div className="card w-25">
-          <div className="card-body ">
-            <h5 className="card-title text-center">ITEMS EN CARRITO</h5>
-            <p className="card-text">
-              Items agregados a carrito: No puede ser mayor que 5 ni menor que 1
-            </p>
-            <div className="d-flex justify-content-between">
-            <a href="#" className="btn btn-primary" onClick={() => this.decrementar()}>
-               - 
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="card w-25">
+        <div className="card-body ">
+          <h5 className="card-title text-center">ITEMS EN CARRITO</h5>
+          <p className="card-text">
+            Items agregados a carrito: No puede ser mayor que 5 ni menor que 1
+          </p>
+          <div className="d-flex justify-content-between">
+            <a
+              href="#"
+              className={contador>1 ? "btn btn-primary" : "btn btn-secondary"}
+              onClick={() => decrementar()}
+            >
+              -
             </a>
-           {this.state.numero}
-           <a href="#" className="btn btn-primary" onClick={() => this.incrementar()}>
+            {contador}
+            <a
+              href="#"
+              className={contador<5 ? "btn btn-primary" : "btn btn-secondary"}
+              onClick={() => incrementar()}
+            >
               +
             </a>
-            
-            </div>
           </div>
         </div>
-        </div>
-      
-      
-    );
-  }
+      </div>
+    </div>
+  );
 }
